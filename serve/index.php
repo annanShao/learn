1ec6ae41a1abec6758337b82f04e2b24f4f58478
annan shao <43042815+annanShao@users.noreply.github.com>
2022-02-14 03:09:32 +0800

    sdfg

--- serve/index.php
+++ serve/index.php
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-13 19:21:17
- * @LastEditTime: 2022-02-14 03:06:24
+ * @LastEditTime: 2022-02-14 03:09:23
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \wxcloudrun-pay-demo-master\server\index.php
@@ -40,10 +40,10 @@
 
   $mchid = '';  // 将子商户ID填写到这里
   $head = getallheaders();
-  if(empty($head['x-wx-source'])){
-    echo sprintf('非法途径');
-    return 100;
-  }
+  // if(empty($head['x-wx-source'])){
+  //   echo sprintf('非法途径');
+  //   return 100;
+  // }
   $body = json_decode(file_get_contents('php://input'),true);
   var_dump($body);
   echo sprintf(file_get_contents('php://input'));
