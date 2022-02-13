7afd1fe064a2582d01a1c89f3eb1da51b62e739d
annan shao <43042815+annanShao@users.noreply.github.com>
2022-02-14 03:25:38 +0800

    sdf

--- serve/index.php
+++ serve/index.php
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-13 19:21:17
- * @LastEditTime: 2022-02-14 03:13:56
+ * @LastEditTime: 2022-02-14 03:25:27
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \wxcloudrun-pay-demo-master\server\index.php
@@ -44,6 +44,7 @@
   //   echo sprintf('非法途径');
   //   return 100;
   // }
+  var_dump($head);
   $body = json_decode(file_get_contents('php://input'),true);
   var_dump($body);
   if($body==null || empty($body["payid"])) {
