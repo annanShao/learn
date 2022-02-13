034f68bab0d202dc6996f0a7ba33f085da4ebf91
annan shao <43042815+annanShao@users.noreply.github.com>
2022-02-14 03:15:53 +0800

    sadf

--- serve/index.php
+++ serve/index.php
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-13 19:21:17
- * @LastEditTime: 2022-02-14 03:09:23
+ * @LastEditTime: 2022-02-14 03:13:56
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \wxcloudrun-pay-demo-master\server\index.php
@@ -46,8 +46,6 @@
   // }
   $body = json_decode(file_get_contents('php://input'),true);
   var_dump($body);
-  echo sprintf(file_get_contents('php://input'));
-  return 200;
   if($body==null || empty($body["payid"])) {
     echo sprintf('没有收到订单ID');
   } else {
